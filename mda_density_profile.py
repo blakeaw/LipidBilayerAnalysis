a1f0a40fa749433e310d6b4e3b9c54d14ef4170a
Blake A. Wilson <blake.wilson@utdallas.edu>
2016-08-05 14:22:31 -0400

    finished electron and added mass density

--- mda_density_profile.py
+++ mda_density_profile.py
@@ -21,11 +21,10 @@ def ElectronDensityProfile(trajectory,mda_selection, fstart=0,fend=-1,fstep=1, a
 		element = atom.name[0]
 		valence = valence_dict[element]
 		partial = atom.charge
-		total = valence+partial
+		total = valence-partial
 		charges[a]=total
 		a+=1
-	zpos_list = []
-	charge_list = []
+	
 	nframes = len(trajectory)
 	if fstart<0:
 		fstart+=nframes
@@ -81,5 +80,81 @@ def ElectronDensityProfile(trajectory,mda_selection, fstart=0,fend=-1,fstep=1, a
 		counts+=counts_f
 	counts/=nframes
 	centers-=reference
-	return counts,centers
+	return centers,counts
+
+def MassDensityProfile(trajectory,mda_selection, fstart=0,fend=-1,fstep=1, axis='z',nbins=100,reference=0.0,refsel=None):
+	lat_ind = [0,1]
+	dir_ind = 2
+	if	axis is 'x':
+		dir_ind=0
+		lat_ind=[1,2]
+	elif axis is 'y':
+		dir_ind=1
+		lat_ind=[0,2]
+	indices = mda_selection.indices
+	natoms = len(mda_selection)
+	#build the mass array
+	masses = np.zeros(natoms)
+	a=0
+	for atom in mda_selection:
+		masses[a]=atom.mass
+		a+=1
+
+	nframes = len(trajectory)
+	if fstart<0:
+		fstart+=nframes
+	if fend < 0:
+		fend+=nframes+1
+	#get the maximum box dimension along axis
+	bzm=0.0
+	nframes = 0
+	sel_z_avg=0.0
+	for frame in trajectory[fstart:fend:fstep]:
+		bzc = frame.dimensions[dir_ind]
+		if bzc>bzm:
+			bzm=bzc
+		ref_sel_z = 0.0
+		if refsel is not None:
+			ref_com = refsel.center_of_mass()
+			ref_sel_z = ref_com[dir_ind]
+			sel_z_avg+=ref_sel_z
+		nframes+=1
+	if refsel is not None:
+		reference=sel_z_avg/nframes
+	#build the profile axis
+	edges = np.linspace(0.0,(bzm),(nbins+1),endpoint=True)
+	incr = edges[1]-edges[0]
+	incr_h = incr/2.0
+	centers = np.zeros(nbins)
+	nedges = len(edges)
+	for i in xrange(1,nedges):
+		j=i-1
+		centers[j]=edges[j]+incr_h
+		
+	counts = np.zeros(nbins)
 	
+	for frame in trajectory[fstart:fend:fstep]:
+		
+		bx = frame.dimensions[lat_ind[0]]
+		by = frame.dimensions[lat_ind[1]]
+		binvolume = incr*bx*by
+		counts_f = np.zeros(nbins)
+		sel_pos = frame._pos[indices]
+		zpos = sel_pos[:,dir_ind]
+		push_index = zpos/incr
+		j=0
+		for i in push_index:
+			ii = int(np.floor(i))
+			if ii >= nbins:
+				ii = nbins-1
+			elif ii < 0:
+				ii = 0
+			counts_f[ii]+=masses[j]
+			j+=1
+		counts_f/=binvolume
+		counts+=counts_f
+	counts/=nframes
+	centers-=reference
+	return centers,counts
+	
+
